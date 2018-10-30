9ac52050bf4f41486f2e808bcf11e2c11534dd21
Rainron <519961939@qq.com>
2018-10-30 15:40:35 +0800

    Initial commit

---
+++ src/com/test/demo/test.java
@@ -0,0 +1,22 @@
+package com.test.demo;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.print("first time to use idea");
+        System.out.print("i never use git");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
